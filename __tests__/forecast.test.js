2fd9ae6191435389cd4793a66061430b0b556f53
Jeff Bloom <bloo0178@gmail.com>
2019-05-15 18:16:25 -0400

    Adding tests. Added Forecast tests.

---
+++ __tests__/forecast.test.js
@@ -0,0 +1,27 @@
+const getForecast = require("../forecast");
+require("dotenv").config();
+
+// http://api.openweathermap.org/data/2.5/weather?lat=41.0262417&lon=-73.62819639999998&APPID=undefined&units=imperial%27,
+
+let lat = 41.0262417;
+let lon = -73.62819639999998;
+let api_key = process.env.WEATHER_API_KEY;
+let units = "imperial";
+
+let result;
+
+describe("Forecast Tests", () => {
+	beforeAll(async () => {
+		result = await getForecast(lat, lon, units, api_key);
+	});
+
+	test("test connection to OpenWeatherMap", () => {
+		console.dir(result);
+		expect(result).toHaveLength(4);
+	});
+
+	test("API returns correct object", () => {
+		expect(result[0]).toHaveProperty("date");
+		expect(result[0]).toHaveProperty("times");
+	});
+});
